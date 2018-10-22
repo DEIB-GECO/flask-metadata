2b43cc018025350f2926bedda05821cbcfc2c1fa
acanakoglu <acanak@gmail.com>
2018-10-22 22:15:22 +0200

    removed duplicated columns from the column list

--- model/utils.py
+++ model/utils.py
@@ -17,6 +17,7 @@ class Column:
 
 def get_column_dict():
     columns = dict()
+    duplicated_columns = []
     for class_name, model_class in inspect.getmembers(models):
         if inspect.isclass(model_class) and issubclass(model_class, db.Model):
             for column_name, column in list(model_class.__dict__.items()):
@@ -27,11 +28,12 @@ def get_column_dict():
                     annotated_col = issubclass(type(exp), sqlalchemy.sql.annotation.AnnotatedColumn)
                     if annotated_col and not primary_key and not foreign_key:
                         if column_name in columns:
-                            # if current_app.
-                            print("multiple "+column_name)
-
+                            # TODO log.WARN
+                            # print("multiple "+column_name)
+                            duplicated_columns.append(column_name)
 
                         columns[column_name] = Column(column_name, model_class.__table__, column, model_class)
+    columns = {k: v for k, v in columns.items() if k not in duplicated_columns}
     return columns
 
 
