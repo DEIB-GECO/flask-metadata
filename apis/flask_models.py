32416982e46d854433de04aed7f49a0b7a43465f
acanakoglu <acanak@gmail.com>
2018-10-24 17:57:08 +0200

    info naming is changed

--- apis/flask_models.py
+++ apis/flask_models.py
@@ -2,7 +2,6 @@ from flask_restplus import Namespace, fields
 
 
 class Info:
-
     def __init__(self, count, total):
         self.count = count
         self.total = total
@@ -11,7 +10,7 @@ class Info:
 api = Namespace('models')
 
 info = api.model('Info', {
-    'count': fields.String(attribute='count', description='TODO'),
-    'total': fields.String(attribute='total', description='TODO'),
+    'shown_count': fields.String(attribute='count', description='TODO'),
+    'total_count': fields.String(attribute='total', description='TODO'),
 
 })
