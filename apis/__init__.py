ce2b8cd2578f5123458a27e0d35e22eeb4308dfa
acanakoglu <acanak@gmail.com>
2018-11-15 17:25:24 +0100

    updated the prefix for the url

--- apis/__init__.py
+++ apis/__init__.py
@@ -3,13 +3,15 @@ from flask_restplus import Api
 
 from .field import api as field_api
 from .flask_models import api as models_api
-from .value import api as value_api
 from .query import api as query_api
+from .value import api as value_api
+
+
 
 # TODO change URL prefix before deploy
-blueprint = Blueprint('api', __name__, url_prefix='/api')
+api_blueprint = Blueprint('api', __name__)
 
-api = Api(blueprint,
+api = Api(api_blueprint,
           title='Metadata API',
           version='1.0',
           description='An API contains Metadata operations',
--- app.py
+++ app.py
@@ -1,42 +1,49 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, Blueprint, url_for
 
-from apis import blueprint
-from model.models import Biosample, db, Project
-from model.utils import column_dict
+from apis import api_blueprint
+from model.models import db
 from utils import get_db_uri
 
-app = Flask(__name__,
-            static_folder='../vue-metadata/dist/static',
-            template_folder='../vue-metadata/dist')
+base_url = '/meta/'
+api_url = base_url + 'api'
 
-app.register_blueprint(blueprint)
+app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = get_db_uri()
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
-app.app_context().push()
-
-# app.logger.debug("testxx")
-#
-# app.logger.debug(Biosample.query.first())
-# app.logger.debug("test2")
-#
-# app.logger.debug(column_dict)
+simple_page = Blueprint('root_pages', __name__,
+                        static_folder='../vue-metadata/dist/static',
+                        template_folder='../vue-metadata/dist')
 
 
-# Make a "catch all route" so all requests match our index.html file. This lets us use the new history APIs in the browser.
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def index(path):
+# base url defined in apis init
+@simple_page.route('/')
+def index():
+    print("serve index")
     return render_template('index.html')
 
 
-if __name__ == '__main__':
-    app.run()
+# Make a "catch all route" so all requests match our index.html file. This lets us use the new history APIs in the browser.
+# @simple_page.route('/', defaults={'path': ''})
+@simple_page.route('/<path:path>')
+def redirect_all(path):
+    return redirect(url_for('.index'))
 
-# prs = Project.query.all()
 
-# for pr in prs:
-#     app.logger.debug(pr.program_name)
-#
-# app.logger.debug(Project.query.all())
+# if __name__ == '__main__':
+#     app.run()
+
+# register blueprints
+app.register_blueprint(api_blueprint, url_prefix=api_url)
+app.register_blueprint(simple_page, url_prefix=base_url)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = get_db_uri()
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+app.app_context().push()
+
+
+# redirect all to base url
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index_all(path):
+    return redirect(base_url)
