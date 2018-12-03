569d5963de000c4f8c0804a09ba48a29dd15250d
acanakoglu <acanak@gmail.com>
2018-12-03 16:04:07 +0100

    changed static url

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ simple_page = Blueprint('root_pages', __name__,
                         static_folder='../vue-metadata/dist/static',
                         template_folder='../vue-metadata/dist')
 
-graph_pages = Blueprint('graph_pages', __name__,
+graph_pages = Blueprint('static', __name__,
                         # static_url_path='/',
                         static_folder='./static/',
                         # template_folder='../vue-metadata/dist'
@@ -35,7 +35,7 @@ def redirect_all(path):
 
 # register blueprints
 my_app.register_blueprint(api_blueprint, url_prefix=api_url)
-my_app.register_blueprint(graph_pages, url_prefix=base_url + "graph")
+my_app.register_blueprint(graph_pages, url_prefix=base_url + "static")
 my_app.register_blueprint(simple_page, url_prefix=base_url)
 my_app.app_context().push()
 
