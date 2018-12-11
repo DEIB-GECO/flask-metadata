6dfbf7e1f33574eebe6b5a485cdbcc7c09d4755c
acanakoglu <acanak@gmail.com>
2018-12-11 13:45:33 +0100

    updated static folder for the graph

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from apis import api_blueprint
 
 base_url = '/repo-viewer/'
 api_url = base_url + 'api'
+graph_static_url = base_url + 'graph_static'
 
 my_app = Flask(__name__)
 
@@ -13,7 +14,7 @@ simple_page = Blueprint('root_pages', __name__,
 
 graph_pages = Blueprint('static', __name__,
                         # static_url_path='/',
-                        static_folder='./static',
+                        static_folder='./graph_static/',
                         # template_folder='../vue-metadata/dist'
                         )
 
@@ -35,7 +36,7 @@ def redirect_all(path):
 
 # register blueprints
 my_app.register_blueprint(api_blueprint, url_prefix=api_url)
-my_app.register_blueprint(graph_pages, url_prefix=base_url + "static")
+my_app.register_blueprint(graph_pages, url_prefix=graph_static_url)
 my_app.register_blueprint(simple_page, url_prefix=base_url)
 my_app.app_context().push()
 
