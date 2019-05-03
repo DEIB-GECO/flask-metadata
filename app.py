ccda6c0812d8cad776f4d8597bc98e889d29e676
acanakoglu <acanak@gmail.com>
2019-05-03 14:49:55 +0200

    updated logger

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def get_db_uri():
 dictConfig({
     'version': 1,
     'formatters': {'default': {
-        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        'format': '[%(asctime)s] %(levelname)s in %(module)s:%(lineno)d: %(message)s',
     }},
     'handlers': {'wsgi': {
         'class': 'logging.StreamHandler',
