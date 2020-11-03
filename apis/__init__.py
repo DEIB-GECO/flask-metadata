8ace01a16de4271cd7539a8bea804d09e10f9244
acanakoglu <acanak@gmail.com>
2020-11-03 18:04:54 +0100

    added last update information

--- apis/__init__.py
+++ apis/__init__.py
@@ -6,6 +6,7 @@ from .flask_models import api as models_api
 from .item import api as item_api
 from .pair import api as pair_api
 from .query import api as query_api
+from .db_info import api as info_api
 
 enable_doc = False
 
@@ -23,6 +24,5 @@ api.add_namespace(models_api)
 
 api.add_namespace(field_api)
 api.add_namespace(query_api)
-# TODO removed item and pair
-# api.add_namespace(item_api)
-# api.add_namespace(pair_api)
+api.add_namespace(info_api)
+
